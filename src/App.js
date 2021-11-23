e296b468edbb09234974e7fe63539ab29e4b8e86
Kelley Worthington <kelleymarne@gmail.com>
2021-11-23 13:53:54 -0700

    fixed dependancy tree issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-
+import React from 'react'
 // import './App.css';
 
 function App() {
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiFillHtml5 } from 'react-icons/ai'
 
 
 function About() {
@@ -13,6 +14,9 @@ function About() {
                     </div>
                     <div className="about-text">
                         My name is Kelley Worthington. I am based out of Salt Lake City, Utah. I'm a brand new junior developer who is very eager to learn and understand. I have always had a love for tech and found a love for HTML/CSS as a young teenager and decided to make the leap and do my best to dive into a bootcamp and learn as much as I can.
+                    </div>
+                    <div>
+                        <AiFillHtml5 />
                     </div>
                 </div>
             </div>
