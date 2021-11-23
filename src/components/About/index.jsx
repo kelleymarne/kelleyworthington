e52df4e9121957780d9a9dac07089d3108d74a87
Kelley Worthington <kelleymarne@gmail.com>
2021-11-23 15:40:47 -0700

    updating skills area

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { AiFillHtml5 } from 'react-icons/ai'
+import { 
+    IoLogoJavascript, 
+    IoLogoReact, 
+    IoLogoNodejs,
+    IoLogoCss3,
+ } from 'react-icons/io5'
 
 
 function About() {
@@ -14,10 +20,13 @@ function About() {
                     </div>
                     <div className="about-text">
                         My name is Kelley Worthington. I am based out of Salt Lake City, Utah. I'm a brand new junior developer who is very eager to learn and understand. I have always had a love for tech and found a love for HTML/CSS as a young teenager and decided to make the leap and do my best to dive into a bootcamp and learn as much as I can.
+                        <br />
+                        <div id="skills">
+                        <AiFillHtml5 style={{fontSize: '2em'}} /> <IoLogoReact style={{fontSize: '2em'}} /> <IoLogoJavascript style={{fontSize: '2em'}} /> <IoLogoCss3 style={{fontSize: '2em'}} />
+                        <IoLogoNodejs style={{fontSize: '2em'}} />
+                        </div>
                     </div>
-                    <div>
-                        <AiFillHtml5 />
-                    </div>
+                
                 </div>
             </div>
         </section>
