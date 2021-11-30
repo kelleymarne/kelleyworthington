6db5654bd1c72f750012713da849f522f4f98e45
Kelley Worthington <kelleymarne@gmail.com>
2021-11-30 00:37:00 -0700

    Updates

---
+++ precache-manifest.f2f272cfc991dc5b096f243b66ff0b70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2460185c2028b840ea7d556cdaf45639",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "8f1ca4acbcb63d1f3bd9",
+    "url": "/reactPortfolio/static/css/main.c020ddcd.chunk.css"
+  },
+  {
+    "revision": "3bb1606f56d4800d25de",
+    "url": "/reactPortfolio/static/js/2.890f5ea6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/reactPortfolio/static/js/2.890f5ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58933198cc9eea598194",
+    "url": "/reactPortfolio/static/js/3.e2f1192c.chunk.js"
+  },
+  {
+    "revision": "8f1ca4acbcb63d1f3bd9",
+    "url": "/reactPortfolio/static/js/main.c81042d3.chunk.js"
+  },
+  {
+    "revision": "3fd297387631bc98a388",
+    "url": "/reactPortfolio/static/js/runtime-main.d0862508.js"
+  },
+  {
+    "revision": "4ce36966589b7934f68d2f93dc80d14c",
+    "url": "/reactPortfolio/static/media/budget.4ce36966.png"
+  },
+  {
+    "revision": "df4eb36d5b0d1e442e63e8b4eb8ba18c",
+    "url": "/reactPortfolio/static/media/carbon.df4eb36d.jpg"
+  },
+  {
+    "revision": "3adfa0c73972c6e28a818a0009140582",
+    "url": "/reactPortfolio/static/media/hero-bg.3adfa0c7.jpg"
+  },
+  {
+    "revision": "616d901eb3ae600e2378448bbbf6e385",
+    "url": "/reactPortfolio/static/media/legoFigures.616d901e.png"
+  },
+  {
+    "revision": "3ecd236f3e31e44d9d498da4a0e8aa0c",
+    "url": "/reactPortfolio/static/media/medium.3ecd236f.jpg"
+  },
+  {
+    "revision": "b1f411665836ec67f49ad439e516c1be",
+    "url": "/reactPortfolio/static/media/sillhouette1.b1f41166.jpg"
+  },
+  {
+    "revision": "d190ab8379f00ad92e2de7fc19483f30",
+    "url": "/reactPortfolio/static/media/taskinator.d190ab83.png"
+  }
+]);
